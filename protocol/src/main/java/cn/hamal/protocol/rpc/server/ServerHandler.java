23f3b258fd10a877ceed1ddb915ea1f672aef4cb
bdqsky <bdqsky@163.com>
2020-02-27 22:49:20 +0800

    fix bug and add log

--- protocol/src/main/java/cn/hamal/protocol/rpc/server/ServerHandler.java
+++ protocol/src/main/java/cn/hamal/protocol/rpc/server/ServerHandler.java
@@ -8,18 +8,24 @@ import cn.hamal.rpc.Response;
 import cn.hamal.rpc.container.ServiceContainer;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author bdq
  * @since 2020/2/25
  */
 public class ServerHandler extends ChannelInboundHandlerAdapter {
+    private static final Logger log = LoggerFactory.getLogger(ServerHandler.class);
     private ServiceContainer serviceContainer = ExtensionLoader.getExtensionLoader(ServiceContainer.class)
             .getDefaultExtension();
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         Request request = (Request) msg;
+        if (log.isDebugEnabled()) {
+            log.debug("recieved request {}!", request.getId());
+        }
 
         Response response = new Response(request.getId(), request.getEvent());
 
